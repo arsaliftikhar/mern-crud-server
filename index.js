18577987e5a5d2fc77244603afe62b67b3c6ceab
arsaliftikhar <65024934+arsaliftikhar@users.noreply.github.com>
2023-08-21 22:43:36 +0500

    env change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const morgan = require("morgan")
 const cors = require("cors")
 
 
@@ -6,8 +7,11 @@ const cors = require("cors")
 const userRoute = require("./routes/users")
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
+const host = process.env.HOST || 'localhost'
 
+
+app.use(morgan("dev"))
 app.use(cors())
 app.use(express.json())
 
@@ -22,7 +26,6 @@ app.use("/",userRoute)
 
 
 
-
-app.listen(5000,()=>{
+app.listen(port,host,()=>{
     console.log(`server is running at ${port}...`);
 })
